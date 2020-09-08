2539a95f42aae8b7ed6157fd25c142b7a352dca0
Faraz Khan <farazrk001@gmail.com>
2020-09-08 21:11:28 +0530

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,77 +1,31 @@
 import React, {useRef} from 'react';
-import {Animated, SafeAreaView, StatusBar, StyleSheet} from 'react-native';
+import {
+  FlatList,
+  View,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+} from 'react-native';
 import Header from './components/Header';
 import ListItem from './components/ListItem';
 import {generateData} from './data';
-import {getCloser} from './utils';
 
-const {diffClamp} = Animated;
 const headerHeight = 58 * 2;
 
 const App = () => {
-  const data = generateData(50);
-
+  const data = generateData(25);
   const ref = useRef(null);
-
-  const scrollY = useRef(new Animated.Value(0));
-  const scrollYClamped = diffClamp(scrollY.current, 0, headerHeight);
-
-  const translateY = scrollYClamped.interpolate({
-    inputRange: [0, headerHeight],
-    outputRange: [0, -(headerHeight / 2)],
-  });
-
-  const translateYNumber = useRef();
-
-  translateY.addListener(({value}) => {
-    translateYNumber.current = value;
-  });
-
-  const handleScroll = Animated.event(
-    [
-      {
-        nativeEvent: {
-          contentOffset: {y: scrollY.current},
-        },
-      },
-    ],
-    {
-      useNativeDriver: true,
-    },
-  );
-
-  const handleSnap = ({nativeEvent}) => {
-    const offsetY = nativeEvent.contentOffset.y;
-    if (
-      !(
-        translateYNumber.current === 0 ||
-        translateYNumber.current === -headerHeight / 2
-      )
-    ) {
-      if (ref.current) {
-        ref.current.scrollToOffset({
-          offset:
-            getCloser(translateYNumber.current, -headerHeight / 2, 0) ===
-            -headerHeight / 2
-              ? offsetY + headerHeight / 2
-              : offsetY - headerHeight / 2,
-        });
-      }
-    }
-  };
 
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar backgroundColor="#1c1c1c" style="light" />
-      <Animated.View style={[styles.header, {transform: [{translateY}]}]}>
+      <View style={[styles.header]}>
         <Header {...{headerHeight}} />
-      </Animated.View>
-      <Animated.FlatList
+      </View>
+      <FlatList
         scrollEventThrottle={16}
         contentContainerStyle={{paddingTop: headerHeight}}
-        onScroll={handleScroll}
         ref={ref}
-        onMomentumScrollEnd={handleSnap}
         data={data}
         renderItem={ListItem}
         keyExtractor={(item, index) => `list-item-${index}-${item.color}`}
